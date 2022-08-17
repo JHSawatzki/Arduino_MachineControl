9c27f946434a6b5b9ce051b3e311a01448578c22
Jan Henrik Sawatzki <JHSawatzki@users.noreply.github.com>
2022-08-17 11:18:58 +0200

    MAX31865 lib changed to original Adafruit lib with additions for async acquisition and further tweaks

--- src/utility/RTD/MAX31865.cpp
+++ src/utility/RTD/MAX31865.cpp
@@ -1,21 +1,36 @@
-/*
-  This file is part of the MachineControl library.
-  Copyright (c) 2021 Arduino SA.
+/***************************************************
+  This is a library for the Adafruit PT100/P1000 RTD Sensor w/MAX31865
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+  Designed specifically to work with the Adafruit RTD Sensor
+  ----> https://www.adafruit.com/products/3328
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+  This sensor uses SPI to communicate, 4 pins are required to
+  interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+/**********Adafruit_MAX31865 modified by Sylvain Boyer 17/02/2021 (https://github.com/sylvanoMTL/Adafruit_MAX31865)
+ * 
+ * 17/02/2021 - added some code from Jack Davies and J-M-L (arduino forum)
+ *  https://forum.arduino.cc/index.php?topic=703346.msg4772457#msg4772457
+ *    added: readRTDAsync() and temperatureAsync
+ *    this allow to keep the code running rather than being stuck by the delays
+ *  17/02/2021 - Fork from https://github.com/budulinek/Adafruit_MAX31865.git
+ *    the fork contains code improvement to allow continuous measurement rather than single shot measurement
+ *    this will improve reading rate but will cause self heating.
+ *    the code also allow the use of the 50Hz filter with the appropriate delay.
+ */
+
+/**
+ * Adaption of the library for the Arduino Portenta Machine Control
+ * and dropped the initial lib from Arduino (which actually used
+ * a lot of code from the original Adafruit lib!)
+ */
 
 #include "MAX31865.h"
 
@@ -25,93 +40,335 @@ MAX31865Class::MAX31865Class(PinName cs) : _spi(SPI), _cs(cs) {
 
 static SPISettings _spiSettings(1000000, MSBFIRST, SPI_MODE1);
 
-bool MAX31865Class::begin(int wires) {
-  _spi.begin();
+/**************************************************************************/
+/*!
+    @brief Initialize the SPI interface and set the number of RTD wires used
+    @param wires The number of wires in enum format. Can be MAX31865_2WIRE,
+    MAX31865_3WIRE, or MAX31865_4WIRE
+    @return True
+*/
+/**************************************************************************/
+bool MAX31865Class::begin(max31865_numwires_t wires) {
+  if(!_begun) {
+    _spi.begin();
 
-  pinMode(_cs, OUTPUT);
-  digitalWrite(_cs, HIGH);
-  // sets 2 or 4 wire
-  if (wires == THREE_WIRE) {
-    writeByte(MAX31856_CONFIG_REG, (readByte(MAX31856_CONFIG_REG) | MAX31856_CONFIG_3_WIRE));
-  } else {
-    writeByte(MAX31856_CONFIG_REG, (readByte(MAX31856_CONFIG_REG) & MAX31856_CONFIG_WIRE_MASK));
+    pinMode(_cs, OUTPUT);
+    digitalWrite(_cs, HIGH);
+
+    setWires(wires);
+    enableBias(false);
+    autoConvert(false);
+    setThresholds(0, 0xFFFF);
+    clearFault();
+    setState(IDLE); // added for asynchronous
+
+    _begun = true;
+    _paused = false;
   }
-
-  // disable bias
-  writeByte(MAX31856_CONFIG_REG, readByte(MAX31856_CONFIG_REG) & MAX31856_CONFIG_BIAS_MASK);
-
-  // disable auto convert mode
-  writeByte(MAX31856_CONFIG_REG, readByte(MAX31856_CONFIG_REG) & MAX31856_CONFIG_CONV_MODE_MASK);
-
-  // clear fault
-  writeByte(MAX31856_CONFIG_REG, (readByte(MAX31856_CONFIG_REG) & MAX31856_CONFIG_CLEAR_FAULT_CYCLE)| MAX31856_CONFIG_CLEAR_FAULT);
-
-  // set filter frequency
-  writeByte(MAX31856_CONFIG_REG, readByte(MAX31856_CONFIG_REG) & MAX31856_CONFIG_60_50_HZ_FILTER_MASK);
 
   return true;
 }
 
+void resume(void) {
+  if (_begun && _paused) {
+    _spi.begin();
+    _paused = false;
+  }
+}
+
+void pause(void) {
+  if (_begun && !_paused) {
+    _spi.end();
+    _paused = true;
+  }
+}
+
+void end(void) {
+  if(_begun) {
+    pause();
+
+    pinMode(_cs, INPUT);
+    digitalWrite(_cs, LOW);
+
+    _begun = false;
+    _paused = false;
+  }
+}
+
+/**************************************************************************/
+/*!
+    @brief Write the lower and upper values into the threshold fault
+    register to values as returned by readRTD()
+    @param lower_threshold raw lower threshold
+    @param upper_threshold raw upper threshold
+*/
+/**************************************************************************/
+void MAX31865Class::setThresholds(uint16_t lower_threshold, uint16_t upper_threshold) {
+  writeByte(MAX31865_LFAULTLSB_REG, lower_threshold & 0xFF);
+  writeByte(MAX31865_LFAULTMSB_REG, lower_threshold >> 8);
+  writeByte(MAX31865_HFAULTLSB_REG, upper_threshold & 0xFF);
+  writeByte(MAX31865_HFAULTMSB_REG, upper_threshold >> 8);
+}
+
+/**************************************************************************/
+/*!
+    @brief Read the raw 16-bit lower threshold value
+    @return The raw unsigned 16-bit value, NOT temperature!
+*/
+/**************************************************************************/
+uint16_t MAX31865Class::getLowerThreshold(void) {
+  return readWord(MAX31865_LFAULTMSB_REG);
+}
+
+/**************************************************************************/
+/*!
+    @brief Read the raw 16-bit lower threshold value
+    @return The raw unsigned 16-bit value, NOT temperature!
+*/
+/**************************************************************************/
+uint16_t MAX31865Class::getUpperThreshold(void) {
+  return readWord(MAX31865_HFAULTMSB_REG);
+}
+
+/**************************************************************************/
+/*!
+    @brief How many wires we have in our RTD setup, can be MAX31865_2WIRE,
+    MAX31865_3WIRE, or MAX31865_4WIRE
+    @param wires The number of wires in enum format
+*/
+/**************************************************************************/
+void MAX31865Class::setWires(max31865_numwires_t wires) {
+  uint8_t config = readByte(MAX31865_CONFIG_REG);
+  if (wires == MAX31865_3WIRE) {
+    config |= MAX31865_CONFIG_3WIRE;
+  } else {
+    // 2 or 4 wire
+    config &= ~MAX31865_CONFIG_3WIRE;
+  }
+  writeByte(MAX31865_CONFIG_REG, config);
+}
+
+/**************************************************************************/
+/*!
+    @brief Whether we want to have continuous conversions (50/60 Hz)
+    @param enabled If true, continuous conversion is enabled
+*/
+/**************************************************************************/
+void MAX31865Class::autoConvert(bool enabled) {
+  uint8_t config = readByte(MAX31865_CONFIG_REG);
+  if (enabled) {
+    config |= MAX31865_CONFIG_MODEAUTO; // enable continuous conversion
+  } else {
+    config &= ~MAX31865_CONFIG_MODEAUTO; // disable continuous conversion
+  }
+  writeByte(MAX31865_CONFIG_REG, config);
+  if (enbaled && !_continuous_mode_enabled) {
+    if (_50hz_filter_enabled) {
+      delay(70);
+    } else {
+      delay(60);
+    } 
+  }
+  _continuous_mode_enabled = enabled;
+}
+
+/**************************************************************************/
+/*!
+    @brief Whether we want filter out 50Hz noise or 60Hz noise
+    @param enabled If true, 50Hz noise is filtered, else 60Hz(default)
+*/
+/**************************************************************************/
+void MAX31865Class::enable50Hz(bool enabled) {
+  uint8_t config = readByte(MAX31865_CONFIG_REG);
+  if (enabled) {
+    config |= MAX31865_CONFIG_FILT50HZ;
+  } else {
+    config &= ~MAX31865_CONFIG_FILT50HZ;
+  }
+  writeByte(MAX31865_CONFIG_REG, config);
+  _50hz_filter_enabled = enabled;
+}
+
+/**************************************************************************/
+/*!
+    @brief Enable the bias voltage on the RTD sensor
+    @param enabled If true bias is enabled, else disabled
+*/
+/**************************************************************************/
+void MAX31865Class::enableBias(bool enabled) {
+  uint8_t config = readByte(MAX31865_CONFIG_REG);
+  if (enabled) {
+    config |= MAX31865_CONFIG_BIAS; // enable bias
+  } else {
+    config &= ~MAX31865_CONFIG_BIAS; // disable bias
+  }
+  writeByte(MAX31865_CONFIG_REG, config);
+  _bias_voltage_enabled = enabled;
+}
+
+/**************************************************************************/
+/*!
+    @brief Read the raw 8-bit FAULTSTAT register
+    @param fault_cycle The fault cycle type to run. Can be MAX31865_FAULT_NONE,
+    MAX31865_FAULT_AUTO, MAX31865_FAULT_MANUAL_RUN, or
+    MAX31865_FAULT_MANUAL_FINISH
+    @return The raw unsigned 8-bit FAULT status register
+*/
+/**************************************************************************/
+uint8_t MAX31865Class::readFault(max31865_fault_cycle_t fault_cycle) {
+  if (fault_cycle) {
+    uint8_t cfg_reg = readByte(MAX31865_CONFIG_REG);
+    cfg_reg &= 0x11; // mask out wire and filter bits
+    switch (fault_cycle) {
+      case MAX31865_FAULT_AUTO:
+        writeByte(MAX31865_CONFIG_REG, (cfg_reg | 0b10000100));
+        delay(1);
+        break;
+      case MAX31865_FAULT_MANUAL_RUN:
+        writeByte(MAX31865_CONFIG_REG, (cfg_reg | 0b10001000));
+        return 0;
+      case MAX31865_FAULT_MANUAL_FINISH:
+        writeByte(MAX31865_CONFIG_REG, (cfg_reg | 0b10001100));
+        return 0;
+      case MAX31865_FAULT_NONE:
+      default:
+        break;
+    }
+  }
+}
+
+/**************************************************************************/
+/*!
+    @brief Clear all faults in FAULTSTAT
+*/
+/**************************************************************************/
 void MAX31865Class::clearFault(void) {
-  writeByte(MAX31856_CONFIG_REG, (readByte(MAX31856_CONFIG_REG) & MAX31856_CONFIG_CLEAR_FAULT_CYCLE) | MAX31856_CONFIG_CLEAR_FAULT);
+  uint8_t config = readByte(MAX31865_CONFIG_REG);
+  config &= ~0x2C;
+  config |= MAX31865_CONFIG_FAULTSTAT;
+  writeByte(MAX31865_CONFIG_REG, config);
 }
 
-uint8_t MAX31865Class::readFault(void) {
-  return readByte(MAX31856_FAULT_STATUS_REG);
-}
-
-bool MAX31865Class::getHighThresholdFault(uint8_t fault) {
-  if (fault & MAX31865_FAULT_HIGH_THRESH) {
-    return true;
+/**************************************************************************/
+/*!
+    @brief Read the raw 16-bit value from the RTD_REG in one shot mode
+    @return The raw unsigned 16-bit value, NOT temperature!
+*/
+/**************************************************************************/
+uint16_t MAX31865Class::readRTD(void) {
+  if (!_continuous_mode_enabled) {
+    if (!_bias_voltage_enabled) {
+      uint8_t config = readByte(MAX31865_CONFIG_REG);
+      config |= MAX31865_CONFIG_BIAS; // enable bias
+      writeByte(MAX31865_CONFIG_REG, config);
+      delay(10);
+    }
+    uint8_t config = readByte(MAX31865_CONFIG_REG);
+    config |= MAX31865_CONFIG_1SHOT;
+    writeByte(MAX31865_CONFIG_REG, config);
+    if (_50hz_filter_enabled) {
+      delay(75);
+    } else {
+      delay(65);
+    }  
   }
-  return false;
-}
 
-bool MAX31865Class::getLowThresholdFault(uint8_t fault) {
-  if (fault & MAX31865_FAULT_LOW_THRESH) {
-    return true;
+  uint16_t rtd_value_raw = readWord(MAX31865_RTDMSB_REG);
+
+  if (!_bias_voltage_enabled) {
+    uint8_t config = readByte(MAX31865_CONFIG_REG);
+    config &= ~MAX31865_CONFIG_BIAS; // disable bias
+    writeByte(MAX31865_CONFIG_REG, config);
   }
-  return false;
+
+  // remove fault
+  rtd_value_raw >>= 1;
+
+  return rtd_value_raw;
 }
 
-bool MAX31865Class::getLowREFINFault(uint8_t fault) {
+/**************************************************************************/
+/*!
+    @brief Read the raw 16-bit value from the RTD_REG in one shot mode but asynchronously using a state machine, 
+    and update the variable rtd_value_raw
+    @param rtd_value_raw The raw unsigned 16-bit value, NOT temperature!
+    @return boolean value: false = conversion not finished, true = conversion finished, the RTD value has been measured
+*/
+/**************************************************************************/
+bool MAX31865Class::readRTDAsync(uint16_t& rtd_value_raw) {
+  bool value_available = false;
 
-  if (fault & MAX31865_FAULT_LOW_REFIN) {
-    return true;
+  switch (_async_state) {
+    case IDLE: // Idle
+      clearFault();
+      if (!_bias_voltage_enabled) {
+        uint8_t config = readByte(MAX31865_CONFIG_REG);
+        config |= MAX31865_CONFIG_BIAS; // enable bias
+        writeByte(MAX31865_CONFIG_REG, config);
+      }
+      _async_timer = millis();
+      _async_state = SETTLING;
+      break;
+
+    case SETTLING: // Bias voltage enabled, waiting to settle
+      if (millis() - _async_timer >= 10) {
+        uint8_t config = readByte(MAX31865_CONFIG_REG);
+        config |= MAX31865_CONFIG_1SHOT;
+        writeByte(MAX31865_CONFIG_REG, config);
+        _async_timer = millis();
+        _async_state = CONVERTING;
+      }
+      break;
+
+    case CONVERTING: // Conversion started, waiting for completion
+      if (millis() - _async_timer >= _50hz_filter_enabled ! 75 : 65) {
+        rtd = readWord(MAX31865_RTDMSB_REG);
+        rtd_value_raw >>= 1; // remove fault
+        _async_state = IDLE; // get ready for next time
+        value_available = true; // signal computation is done
+      }
+      break;
   }
-  return false;
+  return value_available;
 }
 
-bool MAX31865Class::getHighREFINFault(uint8_t fault) {
-
-  if (fault & MAX31865_FAULT_HIGH_REFIN) {
-    return true;
-  }
-  return false;
+/**************************************************************************/
+/*!
+    @brief Read the temperature in C from the RTD through calculation of the
+    resistance. Uses
+   http://www.analog.com/media/en/technical-documentation/application-notes/AN709_0.pdf
+   technique
+    @param rtd_nominal_value The 'nominal' resistance of the RTD sensor, usually 100
+    or 1000
+    @param ref_res_value The value of the matching reference resistor, usually
+    430 or 4300
+    @returns Temperature in C
+*/
+/**************************************************************************/
+float MAX31865Class::readTemperature(float rtd_nominal_value, float ref_res_value) {
+  return calculateTemperature(readRTD(), rtd_nominal_value, ref_res_value);
 }
 
-bool MAX31865Class::getLowRTDINFault(uint8_t fault) {
-
-  if (fault & MAX31865_FAULT_LOW_RTDIN) {
-    return true;
-  }
-  return false;
-}
-
-bool MAX31865Class::getVoltageFault(uint8_t fault) {
-  if (fault & MAX31865_FAULT_OVER_UNDER_VOLTAGE) {
-    return true;
-  }
-  return false;
-}
-
-float MAX31865Class::readTemperature(float RTDnominal, float refResistor) {
+/**************************************************************************/
+/*!
+    @brief Calculate the temperature in C from the RTD through calculation of
+   the resistance. Uses
+   http://www.analog.com/media/en/technical-documentation/application-notes/AN709_0.pdf
+   technique
+    @param rtd_value_raw The raw 16-bit value from the RTD_REG
+    @param rtd_nominal_value The 'nominal' resistance of the RTD sensor, usually 100
+    or 1000
+    @param ref_res_value The value of the matching reference resistor, usually
+    430 or 4300
+    @returns Temperature in C
+*/
+/**************************************************************************/
+float MAX31865Class::calculateTemperature(uint16_t rtd_value_raw, float rtd_nominal_value, float ref_res_value) {
   float Z1, Z2, Z3, Z4, Rt, temp;
 
-  Rt = readRTD();
+  Rt = rtd_value_raw;
   Rt /= 32768;
   Rt *= refResistor;
-
 
   Z1 = -RTD_A;
   Z2 = RTD_A * RTD_A - (4 * RTD_B);
@@ -121,8 +378,9 @@ float MAX31865Class::readTemperature(float RTDnominal, float refResistor) {
   temp = Z2 + (Z3 * Rt);
   temp = (sqrt(temp) + Z1) / Z4;
 
-  if (temp >= 0)
+  if (temp >= 0) {
     return temp;
+  }
 
   // ugh.
   Rt /= RTDnominal;
@@ -144,30 +402,9 @@ float MAX31865Class::readTemperature(float RTDnominal, float refResistor) {
   return temp;
 }
 
-uint32_t MAX31865Class::readRTD() {
-  // clear fault
-  writeByte(MAX31856_CONFIG_REG, (readByte(MAX31856_CONFIG_REG) & MAX31856_CONFIG_CLEAR_FAULT_CYCLE) | MAX31856_CONFIG_CLEAR_FAULT);
-
-  // enable bias
-  writeByte(MAX31856_CONFIG_REG, (readByte(MAX31856_CONFIG_REG) | MAX31856_CONFIG_BIAS_ON));
-  delay(10); // TODO possibly tune delay?
-
-  // ONE shot cOnfIg and make readings change with readByte
-  writeByte(MAX31856_CONFIG_REG, readByte(MAX31856_CONFIG_REG) | MAX31856_CONFIG_ONE_SHOT);
-  delay(65); // TODO possibly tune delay?
-
-  //readings bytes
-  uint16_t read = (readBytes(MAX31856_RTD_MSB_REG));
-  read = read >>1;
-  // disable bias
-  writeByte(MAX31856_CONFIG_REG, readByte(MAX31856_CONFIG_REG) & (MAX31856_CONFIG_BIAS_MASK));
-
-  return read;
-}
-
 uint8_t MAX31865Class::readByte(uint8_t addr) {
-  addr &= 0x7F;
   uint8_t read = 0;
+  addr &= 0x7F;
   digitalWrite(_cs, LOW);
 
   _spi.beginTransaction(_spiSettings);
@@ -180,32 +417,36 @@ uint8_t MAX31865Class::readByte(uint8_t addr) {
   return read;
 }
 
-uint16_t MAX31865Class::readBytes(uint8_t addr) {
+uint16_t MAX31865Class::readWord(uint8_t addr) {
+  uint8_t buffer[2] = {0, 0};
+  addr &= 0x7F; // make sure top bit is not set
+
   digitalWrite(_cs, LOW);
-  uint16_t read = 0x00;
+
   _spi.beginTransaction(_spiSettings);
   _spi.transfer(addr);
-  int i;
-  for (i = 0; i<2; i++) {
-    read = read << 8;
-    read |= _spi.transfer(0);
-  }
-  
+  buffer[0] = _spi.transfer(0x00);
+  buffer[1] = _spi.transfer(0x00);
   _spi.endTransaction();
 
   digitalWrite(_cs, HIGH);
+
+  uint16_t read = buffer[0];
+  read <<= 8;
+  read |= buffer[1];
 
   return read;
 }
 
 void MAX31865Class::writeByte(uint8_t addr, uint8_t data) {
   addr |= 0x80; // make sure top bit is set
+
   uint8_t buffer[2] = {addr, data};
+
   digitalWrite(_cs, LOW);
 
   _spi.beginTransaction(_spiSettings);
-  _spi.transfer(buffer,2);
-
+  _spi.transfer(buffer, 2);
   _spi.endTransaction();
 
   digitalWrite(_cs, HIGH);
--- src/utility/RTD/MAX31865.h
+++ src/utility/RTD/MAX31865.h
@@ -1,21 +1,36 @@
-/*
-  This file is part of the MachineControl library.
-  Copyright (c) 2021 Arduino SA.
+/***************************************************
+  This is a library for the Adafruit PT100/P1000 RTD Sensor w/MAX31865
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+  Designed specifically to work with the Adafruit RTD Sensor
+  ----> https://www.adafruit.com/products/3328
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+  This sensor uses SPI to communicate, 4 pins are required to
+  interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+/**********Adafruit_MAX31865 modified by Sylvain Boyer 17/02/2021 (https://github.com/sylvanoMTL/Adafruit_MAX31865)
+ * 
+ * 17/02/2021 - added some code from Jack Davies and J-M-L (arduino forum)
+ *  https://forum.arduino.cc/index.php?topic=703346.msg4772457#msg4772457
+ *    added: readRTDAsync() and temperatureAsync
+ *    this allow to keep the code running rather than being stuck by the delays
+ *  17/02/2021 - Fork from https://github.com/budulinek/Adafruit_MAX31865.git
+ *    the fork contains code improvement to allow continuous measurement rather than single shot measurement
+ *    this will improve reading rate but will cause self heating.
+ *    the code also allow the use of the 50Hz filter with the appropriate delay.
+ */
+
+/**
+ * Adaption of the library for the Arduino Portenta Machine Control
+ * and dropped the initial lib from Arduino (which actually used
+ * a lot of code from the original Adafruit lib!)
+ */
 
 #ifndef MAX31865_H
 #define MAX31865_H
@@ -24,76 +39,93 @@
 #include <mbed.h>
 #include <SPI.h>
 
-#define MAX31856_CONFIG_REG 0x00
-#define MAX31856_RTD_MSB_REG 0x01
-#define MAX31856_FAULT_STATUS_REG 0x07
+#define MAX31865_CONFIG_REG 0x00
+#define MAX31865_CONFIG_BIAS 0x80
+#define MAX31865_CONFIG_MODEAUTO 0x40
+#define MAX31865_CONFIG_MODEOFF 0x00
+#define MAX31865_CONFIG_1SHOT 0x20
+#define MAX31865_CONFIG_3WIRE 0x10
+#define MAX31865_CONFIG_24WIRE 0x00
+#define MAX31865_CONFIG_FAULTSTAT 0x02
+#define MAX31865_CONFIG_FILT50HZ 0x01
+#define MAX31865_CONFIG_FILT60HZ 0x00
 
-//config bias mask
-#define MAX31856_CONFIG_BIAS_MASK 0x7F
-#define MAX31856_CONFIG_BIAS_ON 0x80
+#define MAX31865_RTDMSB_REG 0x01
+#define MAX31865_RTDLSB_REG 0x02
+#define MAX31865_HFAULTMSB_REG 0x03
+#define MAX31865_HFAULTLSB_REG 0x04
+#define MAX31865_LFAULTMSB_REG 0x05
+#define MAX31865_LFAULTLSB_REG 0x06
+#define MAX31865_FAULTSTAT_REG 0x07
 
-//config conversion mode mask
-#define MAX31856_CONFIG_CONV_MODE_MASK 0xBF
-#define MAX31856_CONFIG_CONV_MODE_AUTO 0x40
-
-//config one shot mask
-#define MAX31856_CONFIG_ONE_SHOT_MASK 0xDF
-#define MAX31856_CONFIG_ONE_SHOT 0x20
-
-//config wire mask
-#define MAX31856_CONFIG_WIRE_MASK 0xEF
-#define MAX31856_CONFIG_3_WIRE 0x10
-
-//config wire fault detection cycle mask
-#define MAX31856_CONFIG_FAULT_DECT_CYCLE_MASK 0xF3
-#define MAX31856_CONFIG_CLEAR_FAULT_CYCLE 0xD3
-
-//config fault status mask
-#define MAX31856_CONFIG_FAULT_STATUS_MASK 0xFD
-#define MAX31856_CONFIG_CLEAR_FAULT 0x02
-
-// config 50 60 filter frequency mask
-#define MAX31856_CONFIG_60_50_HZ_FILTER_MASK 0xFE
-
-// fault mask
-#define MAX31865_FAULT_HIGH_THRESH 0x80
-#define MAX31865_FAULT_LOW_THRESH 0x40
-#define MAX31865_FAULT_LOW_REFIN 0x20
-#define MAX31865_FAULT_HIGH_REFIN 0x10
-#define MAX31865_FAULT_LOW_RTDIN 0x08
-#define MAX31865_FAULT_OVER_UNDER_VOLTAGE 0x04
+#define MAX31865_FAULT_HIGHTHRESH 0x80
+#define MAX31865_FAULT_LOWTHRESH 0x40
+#define MAX31865_FAULT_REFINLOW 0x20
+#define MAX31865_FAULT_REFINHIGH 0x10
+#define MAX31865_FAULT_RTDINLOW 0x08
+#define MAX31865_FAULT_OVUV 0x04
 
 #define RTD_A 3.9083e-3
 #define RTD_B -5.775e-7
 
-#define TWO_WIRE 0
-#define THREE_WIRE 1
+typedef enum max31865_numwires {
+  MAX31865_2WIRE = 0,
+  MAX31865_3WIRE = 1,
+  MAX31865_4WIRE = 0
+} max31865_numwires_t;
+
+enum t_state : byte {IDLE, SETTLING, CONVERTING}; // for asynchronous mode
+
+typedef enum {
+  MAX31865_FAULT_NONE = 0,
+  MAX31865_FAULT_AUTO,
+  MAX31865_FAULT_MANUAL_RUN,
+  MAX31865_FAULT_MANUAL_FINISH
+} max31865_fault_cycle_t;
 
 class MAX31865Class {
 public:
   MAX31865Class(PinName cs = PA_6);
 
-  bool begin(int wires);
-  void end();
+  bool begin(max31865_numwires_t wires = MAX31865_2WIRE);
+  void resume(void);
+  void pause(void);
+  void end(void);
 
-  float readTemperature(float RTDnominal, float refResistor);
-  uint8_t readFault(void);
+  void setThresholds(uint16_t lower_threshold, uint16_t upper_threshold);
+  uint16_t getLowerThreshold(void);
+  uint16_t getUpperThreshold(void);
+
+  void setWires(max31865_numwires_t wires);
+  void setAutoConvert(bool enabled);
+  void set50HzFilter(bool enabled);
+  void setBias(bool enabled);
+
+  uint8_t readFault(max31865_fault_cycle_t fault_cycle = MAX31865_FAULT_AUTO);
   void clearFault(void);
-  uint32_t readRTD();
-  bool getHighThresholdFault(uint8_t fault);
-  bool getLowThresholdFault(uint8_t fault);
-  bool getLowREFINFault(uint8_t fault);
-  bool getHighREFINFault(uint8_t fault);
-  bool getLowRTDINFault(uint8_t fault);
-  bool getVoltageFault(uint8_t fault);
+
+  uint16_t readRTD(void);
+  bool readRTDAsync(uint16_t& rtd_value_raw); //added by JD modified by Sylvain Boyer
+
+  float readTemperature(float rtd_nominal_value, float ref_res_value);
+
+  float calculateTemperature(uint16_t rtd_value_raw, float rtd_nominal_value, float ref_res_value);
 
 private:
   uint8_t readByte(uint8_t addr);
-  uint16_t readBytes(uint8_t addr);
+  uint16_t readWord(uint8_t addr);
   void writeByte(uint8_t addr, uint8_t data);
 
   PinName _cs;
   SPIClass& _spi;
+
+  bool _begun;
+  bool _paused;
+  bool _continuous_mode_enabled; // continuous conversion
+  bool _50hz_filter_enabled; // 50Hz filter
+  bool _bias_voltage_enabled; // bias voltage
+  uint32_t _async_timer; // timer for asynchronous mode, added Sylvain Boyer
+  t_state _async_state; // state for asynchronous mode //Does static matter?
 };
 
 #endif
